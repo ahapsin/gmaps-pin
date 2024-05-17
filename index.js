2cd7988fc3293a3509165c5febc97ebe9586bd8a
ahapsin <ahapsin02@gmail.com>
2024-05-17 17:25:22 +0700

    last

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 //adding Puppeteer library
 const pt = require("puppeteer");
+const express = require("express");
+const app = express();
+const port = 3000;
+
 pt.launch({ headless: false }).then(async (browser) => {
   //browser new page
   const p = await browser.newPage();
@@ -7,9 +11,15 @@ pt.launch({ headless: false }).then(async (browser) => {
   await p.setViewport({ width: 1000, height: 500 });
   //launch URL
   await p.goto("https://www.google.com/maps");
-  await p.waitForSelector(".t9f27");
-  const spanVal = await p.$eval(".t9f27", (el) => el.innerText);
-  console.log(spanVal);
+  //   await p.waitForSelector(".t9f27");
+  //   const spanVal = await p.$eval(".t9f27", (el) => el.innerText);
 
-  await browser.close();
+  //   app.get("/", (req, res) => {
+  //     res.send(spanVal);
+  //   });
+  //   await browser.close();
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
